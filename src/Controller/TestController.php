1d4c7604e532027686b4669bdf69911ea590461d
martin_kobelka <martinkobelka@gmail.com>
2022-02-21 22:08:21 +0100

    All

---
+++ src/Controller/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestController extends AbstractController
+{
+    #[Route('/api', name: 'api')]
+    public function index(): Response
+    {
+        $response = new Response();
+        $response->setContent(json_encode([
+            'data' => 123,
+        ]));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+}
